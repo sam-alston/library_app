780e7f75947ed4f14db7d887fd5ffcc0dca38dfb
sam-alston <arching0@gmail.com>
2018-04-06 07:18:29 -0700

    Added scripts for area_populate.sql and room_populate.sql

---
+++ load-demo/source/scripts/area_populate.sql
@@ -0,0 +1,692 @@
+--This is the Main lobby (Area 1, Floor 1)
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values 
+(1, 75, 257);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(1, 75, 322);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(1, 189, 322);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(1, 189, 257);
+
+
+--This is the Cafe (Area 2, Floor 1)
+insert into `hsu_library`.``hsu_library`.`area_vertices``
+(`area_id`, `v_x`, `v_y`)
+values
+(2, 200, 258);
+
+insert into `hsu_library`.``hsu_library`.`area_vertices``
+(`area_id`, `v_x`, `v_y`)
+values
+(2, 200, 291);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(2, 189, 297);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(2, 189, 315);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(2, 200, 323);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(2, 200, 356);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(2, 240, 356);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(2, 240, 258);
+
+
+--This is the Learning Center and Tutoring Area (Area 3, Floor 1)
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(3, 269, 191);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(3, 338, 191);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(3, 338, 56);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(3, 269, 56);
+
+
+--This is the Computer Lab (Area 4, Floor 1)
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(4, 170, 126);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(4, 140, 126);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(4, 140, 192);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(4, 205, 192);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(4, 205, 258);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(4, 235, 258);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(4, 235, 224);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(4, 269, 224);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(4, 269, 56);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(4, 170, 56);
+
+
+--This is the Quiet Study Area (Area 5, Floor 2)
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 101, 61);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 101, 94);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 105.5, 94);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 105.5, 126.5);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 101, 126.5);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 101, 159.2);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 105.5, 159.2);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 105.5, 191);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 115, 191);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 115, 75);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 319, 75);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 319, 191);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 334, 191);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 334, 159.2);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 339, 159.2);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 339, 126.6);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 334, 126.6);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 334, 57);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 301.5, 57);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 301.5, 61.3);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 268.9, 61.3);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 268.9, 57);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 236.2, 57);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 236.2, 61.3);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 203.4, 61.3);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 203.4, 57);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 170.7, 57);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(5, 170.7, 61);
+
+
+--This is the SW Group Study (Area 6, Floor 2)
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(6, 206, 191);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(6, 206, 223);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(6, 302, 223);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(6, 302, 229);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(6, 333, 229);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(6, 333, 224);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(6, 338, 224);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(6, 338, 191);
+
+
+--This is the West Window Group Study Area (Area 7, Floor 2)
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(7, 205, 349);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(7, 240, 349);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(7, 240, 257);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(7, 205, 257);
+
+
+--This is the Collaboration Lab(Area 8, Floor 2)
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(8, 205, 257);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(8, 191, 257);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(8, 191, 354);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(8, 205, 354);
+
+
+--This is the Whiteboard Group Study Area(Area 9, Floor 2)
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(9, 191, 265);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(9, 152, 265);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(9, 152, 440);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(9, 191, 440);
+
+
+--This is the Children's Literature Group Study(Area 10, Floor 2)
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(10, 281, 454);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(10, 281, 484);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(10, 286, 484);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(10, 286, 497);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(10, 280, 497);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(10, 280, 502);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(10, 268, 502);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(10, 268, 497);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(10, 236, 497);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(10, 236, 502);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(10, 203, 502);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(10, 203, 454);
+
+
+--This is the Helen Everett Reading Room Group Study Area (Area 11, Floor 2)
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(11, 60, 497);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(11, 60, 502);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(11, 203, 502);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(11, 203, 461);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(11, 96, 461);
+
+
+--This is the HSU Authors Hall Group Study(Area 12, Floor 2)
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(12, 60, 497);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(12, 55, 497);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(12, 55, 258);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(12, 104, 258);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(12, 104, 443);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(12, 96, 443);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(12, 96, 461);
+
+
+--This is the Quiet Area (Area 13, Floor 3)
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(13, 61, 61);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(13, 61, 204);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(13, 75, 204);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(13, 75, 75);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(13, 298, 75);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(13, 298, 170);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(13, 312, 170);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(13, 312, 61);
+
+
+--This is the Scholars Runway (Area 14, Floor 3)
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(14, 167, 227);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(14, 60, 276);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(14, 60, 298);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(14, 141, 298);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(14, 141, 468);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(14, 97, 468);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(14, 97, 494);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(14, 167, 494);
+
+
+--This is the Special Collections (Area 15, Floor 3)
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(15, 60, 298);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(15, 141, 298);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(15, 141, 421);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(15, 60, 421);
+
+
+--This is the Scholars Lab (Area 16, Floor 3)
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(16, 167, 277);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(16, 167, 400);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(16, 194, 400);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(16, 194, 385);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(16, 209, 385);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(16, 209, 277);
+
+
+--This is the Social Group Study (Area 17, Floor 3)
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(17, 171, 240);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(17, 171, 75);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(17, 197, 75);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(17, 197, 215);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(17, 250, 215);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(17, 250, 170);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(17, 276, 170);
+
+insert into `hsu_library`.`area_vertices`
+(`area_id`, `v_x`, `v_y`)
+values
+(17, 276, 240);
